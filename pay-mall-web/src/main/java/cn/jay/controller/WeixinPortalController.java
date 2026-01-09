9c8012435d4198ddd7470b873ed093ecb7ad74a6
gongwenjiejay <gwj1121334089@gmail.com>
2026-01-09 09:54:01 +0800

    Update WeixinPortalController.java

--- pay-mall-web/src/main/java/cn/jay/controller/WeixinPortalController.java
+++ pay-mall-web/src/main/java/cn/jay/controller/WeixinPortalController.java
@@ -14,7 +14,6 @@ import javax.annotation.Resource;
 /**
  * 微信服务对接，对接地址：<a href="http://gwj-mall.natapp1.cc/api/v1/weixin/portal/receive">/api/v1/weixin/portal/receive</a>
  * <p>
- * http://xfg-studio.natapp1.cc/api/v1/weixin/portal/receive/
  */
 @Slf4j
 @RestController()
