c655ff556c5358d1f34746384c1d15feade82362
seattleiscoming <1121334089@qq.com>
2026-01-09 09:56:43 +0800

    product-update

--- pay-mall-service/src/main/java/cn/jay/service/ILoginService.java
+++ pay-mall-service/src/main/java/cn/jay/service/ILoginService.java
@@ -2,11 +2,6 @@ package cn.jay.service;
 
 import java.io.IOException;
 
-/**
- * @author Fuzhengwei bugstack.cn @小傅哥
- * @description 微信服务
- * @create 2024-09-28 13:44
- */
 public interface ILoginService {
 
     String createQrCodeTicket() throws Exception;
